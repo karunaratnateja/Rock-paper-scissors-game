12662034d43d7cd3f5b0ffdc664a753dafaa00d4
karunaratnateja <karunaratnateja@gmail.com>
2026-04-08 21:11:44 +0530

    Add files via upload

---
+++ pro02.cpp
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+    int user, computer;
+
+    printf("=== Rock Paper Scissors Game ===\n");
+    printf("1. Rock\n2. Paper\n3. Scissors\n");
+
+    printf("Enter your choice (1-3): ");
+    scanf("%d", &user);
+
+    // Generate random number for computer
+    srand(time(0));
+    computer = rand() % 3 + 1;
+
+    printf("Computer chose: ");
+    if (computer == 1) printf("Rock\n");
+    else if (computer == 2) printf("Paper\n");
+    else printf("Scissors\n");
+
+    // Game logic
+    if (user == computer) {
+        printf("It's a Draw!\n");
+    }
+    else if ((user == 1 && computer == 3) ||
+             (user == 2 && computer == 1) ||
+             (user == 3 && computer == 2)) {
+        printf("You Win! ??\n");
+    }
+    else if (user >= 1 && user <= 3) {
+        printf("Computer Wins! ??\n");
+    }
+    else {
+        printf("Invalid choice!\n");
+    }
+
+    return 0;
+}
